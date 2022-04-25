2a55e58dfc3d47ff668e60d2fa7045e4952dda40
VakhtangTsintsadze <vatotsintsadzee@gmail.com>
2022-04-25 20:19:44 +0400

    Adding Homework2 from NewBranch

---
+++ Homework2/Visual Studio Solution/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Vakhtang");
+Console.WriteLine("Tsintsadze");
+Console.WriteLine("-------------");
+
+
